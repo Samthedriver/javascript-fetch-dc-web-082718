996dccb1bdb5b36197e7cafd96a14ded47c6bff0
github username <github email address>
2018-10-13 09:30:15 -0400

    Accessing API with with fetch.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much.";
+
+const token = '163d8af7ee9858cef2cab02b9c8a9977dc5edaae'
+fetch('https://api.github.com/repos/jquery/jquery/commits',
+  {
+    headers:
+      {
+        Authorization: `token ${token}`
+      }
+  }).then(res => res.json()).then(json => console.log(json));
+
+//   const token = 'YOUR_TOKEN_HERE'
+// fetch('https://api.github.com/user/repos', {
+//   headers: {
+//     Authorization: `token ${token}`
+//   }
+// }).then(res => res.json()).then(json => console.log(json));
